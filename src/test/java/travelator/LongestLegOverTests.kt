3ce22a1b6f47868a837d6ff08a8a7d4be8833e49
Duhan Mo <devduhanmo@gmail.com>
2023-02-19 20:49:34 +0900

    4장: 테스트 클래스를 모두 코틀린 인터페이스 널허용 리턴값 메서드를 이용

--- src/test/java/travelator/LongestLegOverTests.kt
+++ src/test/java/travelator/LongestLegOverTests.kt
@@ -1,8 +1,10 @@
 package travelator
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import travelator.Legs.findLongestLegOver
+import travelator.Legs.longestLegOver
 import java.time.Duration
 import java.time.Instant
 import java.time.ZoneId
@@ -18,37 +20,32 @@ class LongestLegOverTests {
         leg("two hours", Duration.ofHours(2))
     )
     private val oneDay = Duration.ofDays(1)
+
     @Test
-    fun is_absent_when_no_legs() {
-        Assertions.assertEquals(
-            Optional.empty<Any>(),
-            findLongestLegOver(emptyList(), Duration.ZERO)
-        )
+    fun `is absent when no legs`() {
+        assertNull(longestLegOver(emptyList(), Duration.ZERO))
     }
 
     @Test
-    fun is_absent_when_no_legs_long_enough() {
-        Assertions.assertEquals(
-            Optional.empty<Any>(),
-            findLongestLegOver(legs, oneDay)
-        )
+    fun `is absent when no legs long enough`() {
+        assertNull(longestLegOver(legs, oneDay))
     }
 
     @Test
-    fun is_longest_leg_when_one_match() {
-        Assertions.assertEquals(
+    fun `is longest leg when one match`() {
+        assertEquals(
             "one day",
-            findLongestLegOver(legs, oneDay.minusMillis(1))
-                .orElseThrow().description
+            longestLegOver(legs, oneDay.minusMillis(1))
+            !!.description
         )
     }
 
     @Test
-    fun is_longest_leg_when_more_than_one_match() {
-        Assertions.assertEquals(
+    fun `is longest leg when more than one match`() {
+        assertEquals(
             "one day",
-            findLongestLegOver(legs, Duration.ofMinutes(59))
-                .orElseThrow().description
+            longestLegOver(legs, Duration.ofMinutes(59))
+                ?.description
         )
     }
 
