b2a40fdbb6843c930c84f62122a2b7c4537e7164
Duhan Mo <devduhanmo@gmail.com>
2023-02-19 21:02:55 +0900

    4장: 알고리즘을 파악후 maxByOrNull() 컬렉션함수 사용

--- src/main/java/travelator/Legs.kt
+++ src/main/java/travelator/Legs.kt
@@ -3,18 +3,9 @@ package travelator
 import java.time.Duration
 
 fun longestLegOver(legs: List<Leg>, duration: Duration): Leg? {
-    var result: Leg? = null
-    for (leg in legs) {
-        if (leg.isLongerThan(duration)) {
-            if (result == null ||
-                leg.isLongerThan(result.plannedDuration)
-            ) {
-                result = leg
-            }
-        }
-    }
-    return result
+    val longestLeg: Leg? = legs.maxByOrNull(Leg::plannedDuration)
+    return if (longestLeg != null && longestLeg.plannedDuration > duration)
+        longestLeg
+    else
+        null
 }
-
-private fun Leg.isLongerThan(duration: Duration) =
-    plannedDuration > duration
