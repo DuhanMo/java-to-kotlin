31a01726304873409808516d8de5ec0495bc0913
Duhan Mo <devduhanmo@gmail.com>
2023-03-05 20:17:37 +0900

    5장: UserPreferences를 데이터 클래스로 변경

--- src/main/java/travelator/mobile/UserPreferences.kt
+++ src/main/java/travelator/mobile/UserPreferences.kt
@@ -2,8 +2,8 @@ package travelator.mobile
 
 import java.util.*
 
-class UserPreferences @JvmOverloads constructor(
-    @JvmField var greeting: String = "Hello",
-    @JvmField var locale: Locale = Locale.UK,
-    @JvmField var currency: Currency = Currency.getInstance(Locale.UK)
+data class UserPreferences(
+    val greeting: String,
+    val locale: Locale,
+    val currency: Currency
 )
