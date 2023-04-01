8e508fdd8940825391c473312f453059b6731c6d
Duhan Mo <devduhanmo@gmail.com>
2023-04-01 23:45:41 +0900

    9장: 인라이닝과 새 함수 도입을 통한 단일식으로 만들기 1

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -1,24 +1,24 @@
 package travelator
 
-import java.util.*
-
 data class EmailAddress(
     val localPart: String,
     val domain: String
 ) {
 
-    override fun toString(): String {
-        return "$localPart@$domain"
-    }
+    override fun toString(): String = "$localPart@$domain"
 
     companion object {
-        fun parse(value: String): EmailAddress {
-            val atIndex = value.lastIndexOf('@')
-            require(!(atIndex < 1 || atIndex == value.length - 1)) { "EmailAddress must be two parts separated by @" }
-            return EmailAddress(
-                value.substring(0, atIndex),
-                value.substring(atIndex + 1)
-            )
-        }
+        fun parse(value: String): EmailAddress =
+            emailAddress(value, value.lastIndexOf('@'))
+
+        private fun emailAddress(value: String, atIndex: Int): EmailAddress =
+            if ((atIndex < 1 || atIndex == value.length - 1)) {
+                throw IllegalArgumentException("EmailAddress must be two parts separated by @")
+            } else {
+                EmailAddress(
+                    value.substring(0, atIndex),
+                    value.substring(atIndex + 1)
+                )
+            }
     }
 }
