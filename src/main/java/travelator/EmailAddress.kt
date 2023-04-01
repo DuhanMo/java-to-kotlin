c68827703ad8a5f7dcb1bec2c26afd2bde04f8e2
Duhan Mo <devduhanmo@gmail.com>
2023-04-02 00:19:24 +0900

    9장: *혼자실습* throw는 Nothing 타입이므로 null로 처리! (자바클라이언트를 논외로 함)

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -8,18 +8,19 @@ data class EmailAddress(
     override fun toString(): String = "$localPart@$domain"
 
     companion object {
-        fun parse(value: String): EmailAddress =
+        fun parse(value: String): EmailAddress? =
             value.splitAroundLast('@')
-                .let { (leftPart, rightPart) ->
+                ?.let { (leftPart, rightPart) ->
                     EmailAddress(leftPart, rightPart)
                 }
 
-        private fun String.splitAroundLast(divider: Char): Pair<String, String> =
+        private fun String.splitAroundLast(divider: Char): Pair<String, String>? =
             lastIndexOf(divider).let { atIndex ->
-                require(!(atIndex < 1 || atIndex == length - 1)) {
-                    "EmailAddress must be two parts separated by $divider"
+                if ((atIndex < 1 || atIndex == length - 1)) {
+                    return null
+                } else {
+                    substring(0, atIndex) to substring(atIndex + 1)
                 }
-                substring(0, atIndex) to substring(atIndex + 1)
             }
     }
 }
