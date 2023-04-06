55513592661387ee33c9f9eafd4f5078e6b7309e
Duhan Mo <devduhanmo@gmail.com>
2023-04-06 14:10:57 +0900

    13장: 시퀀스 고려, typealias, 인라이닝으로 리팩터링

--- src/main/java/travelator/analytics/MarketingAnalytics.kt
+++ src/main/java/travelator/analytics/MarketingAnalytics.kt
@@ -1,26 +1,35 @@
 package travelator.analytics
 
-import java.util.stream.Collectors.groupingBy
+import kotlin.streams.asSequence
 
 class MarketingAnalytics(
     private val eventStore: EventStore
 ) {
     fun averageNumberOfEventsPerCompletedBooking(
         timeRange: String
-    ): Double {
-        val eventsForSuccessfulBookings = eventStore
-            .queryAsStream("type=CompletedBooking&timerange=$timeRange")
-            .flatMap { event ->
-                val interactionId = event["interactionId"] as String
-                eventStore.queryAsStream("interactionId=$interactionId")
-            }
-        val bookingEventsByInteractionId = eventsForSuccessfulBookings.collect(
-            groupingBy { event -> event["interactionId"] as String }
-        )
-        val values = bookingEventsByInteractionId.values
-        return values.averageBy { it.size }
-    }
+    ): Double = eventStore
+        .queryAsSequence("type=CompletedBooking&timerange=$timeRange")
+        .allEventsInSameInteractions()
+        .groupBy(Event::interactionId)
+        .values
+        .averageBy { it.size }
 
-    private fun <T> Collection<T>.averageBy(selector: (T) -> Int) =
-        sumOf(selector) / size.toDouble()
+    private fun Sequence<Event>.allEventsInSameInteractions() =
+        flatMap { event ->
+            eventStore.queryAsSequence(
+                "interactionId=${event.interactionId}"
+            )
+        }
 }
+
+inline fun <T> Collection<T>.averageBy(selector: (T) -> Int): Double =
+    sumOf(selector) / size.toDouble()
+
+fun EventStore.queryAsSequence(query: String) =
+    this.queryAsStream(query).asSequence()
+
+typealias Event = Map<String, Any?>
+
+val Event.interactionId: String?
+    get() =
+        this["interactionId"] as? String
